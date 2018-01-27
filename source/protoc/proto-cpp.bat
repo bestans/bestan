5920f39e144825f2eb3376e9611278e6a256fa0e
bestans <632469297@qq.com>
2018-01-27 18:27:03 +0800

    生成c# proto文件

--- source/protoc/proto-cpp.bat
+++ source/protoc/proto-cpp.bat
@@ -12,5 +12,8 @@ set DST_DIR=%CurrentDir%\..\
 %protoExe% -I=%SRC_DIR% --java_out=%DST_DIR% %SRC_DIR%\net_common.proto
 %protoExe% -I=%SRC_DIR% --java_out=%DST_DIR% %SRC_DIR%\net_base.proto
 
+%protoExe% -I=%SRC_DIR% --csharp_out=%DST_DIR% %SRC_DIR%\net_common.proto
+%protoExe% -I=%SRC_DIR% --csharp_out=%DST_DIR% %SRC_DIR%\net_base.proto
+
 echo Success
 pause
