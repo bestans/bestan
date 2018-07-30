db87c8c96f9cbe3d5ff0ade75df0f9ac597aea7b
bestans <632469297@qq.com>
2018-07-30 17:53:29 +0800

    提交

--- bestan-log/src/bestan/log/MyAppender.java
+++ bestan-log/src/bestan/log/MyAppender.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Priority;
 public class MyAppender extends FileAppender {
 	@Override
 	public boolean isAsSevereAsThreshold(Priority priority) {
-        // 只判断是否相等，而不判断优先级
+		// 只判断是否相等，而不判断优先级
         return this.getThreshold().equals(priority);
     }
 }
--- bestan-log/src/bestan/log/Test.java
+++ bestan-log/src/bestan/log/Test.java
@@ -3,11 +3,11 @@ package bestan.log;
 public class Test {
 	public static void test2()
 	{
-		GLog.log.trace("is customize trace log {} data {}", 9, "adfasfd");
-		GLog.log.debug("is customize debug log {} data", 10);
-		GLog.log.info("is customize info log {} data", 11);
-		GLog.log.warn("is customize warn log {} data", 12);
-		GLog.log.error("is customize error log {} data", 13);
+		Glog.log.trace("is customize trace log {} data {}", 9, "adfasfd");
+		Glog.log.debug("is customize debug log {} data", 10);
+		Glog.log.info("is customize info log {} data", 11);
+		Glog.log.warn("is customize warn log {} data", 12);
+		Glog.log.error("is customize error log {} data", 13);
 	}
 	public static void main(String[] args) {
 		test2();
