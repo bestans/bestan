3e9f437ae2b797182214fd32a16a24cef3f4531e
bestans <632469297@qq.com>
2018-09-06 01:19:17 +0800

    xxxx

---
+++ bestan-common/src/test/java/bestan/test/server/BaseProtoHandler.java
@@ -0,0 +1,33 @@
+package bestan.test.server;
+
+import bestan.common.log.Glog;
+import bestan.common.message.IMessageHandler;
+import bestan.common.net.AbstractProtocol;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+/**
+ * @author yeyouhuan
+ *
+ */
+public class BaseProtoHandler implements IMessageHandler {
+
+	@Override
+	public void processProtocol(AbstractProtocol protocol) {
+		Glog.debug("BaseProtoHandle:message={}", protocol.getMessage());
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		var channelFuture = protocol.getChannelHandlerContext().writeAndFlush(protocol.getMessage());
+		channelFuture.addListener(new GenericFutureListener<Future<? super Void>>() {
+
+			@Override
+			public void operationComplete(Future<? super Void> future) throws Exception {
+				Glog.debug("BaseProtoHandler:future={}", future);
+			}
+		});
+	}
+}
