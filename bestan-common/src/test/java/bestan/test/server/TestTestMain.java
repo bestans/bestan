f20d817cff7279c61fbc4c77013d94a028bfbd3f
bestans <632469297@qq.com>
2018-09-18 21:04:53 +0800

    xx

--- bestan-common/src/test/java/bestan/test/server/TestTestMain.java
+++ bestan-common/src/test/java/bestan/test/server/TestTestMain.java
@@ -2,6 +2,8 @@ package bestan.test.server;
 
 import java.util.concurrent.Executors;
 
+import org.apache.commons.mail.HtmlEmail;
+
 import bestan.common.event.IEvent;
 import bestan.common.log.Glog;
 import bestan.common.logic.FormatException;
@@ -141,9 +143,35 @@ public class TestTestMain {
 		}
 		timer.close();
 	}
+
+
+	//邮箱验证码
+	public static boolean sendEmail(String emailaddress,String code){
+		try {
+			HtmlEmail email = new HtmlEmail();//不用更改
+			email.setHostName("smtp.163.com");//需要修改，126邮箱为smtp.126.com,163邮箱为163.smtp.com，QQ为smtp.qq.com
+			email.setCharset("UTF-8");
+			email.addTo(emailaddress);// 收件地址
+ 
+			email.setFrom("youhuanye@163.com", "aa");//此处填邮箱地址和用户名,用户名可以任意填写
+ 
+			email.setAuthentication("youhuanye@163.com", "tianya1988188"); //此处填写邮箱地址和客户端授权码
+ 
+			email.setSubject("孙大大通讯");//此处填写邮件名，邮件名可任意填写
+			email.setMsg("尊敬的用户您好,您本次注册的验证码是:" + code);//此处填写邮件内容
+ 
+			email.send();
+			return true;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
 	public static void main(String[] args) {
 		try {
-			test6();
+			sendEmail("632469297@qq.com", "100");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
